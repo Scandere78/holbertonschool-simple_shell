4c423c0e112fef6485b56f514fbf9332f765847c
Scandere78 <scanderetejpro@gmail.com>
2024-04-18 14:10:06 +0200

    function

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define MAX_COMMAND_LENGTH 100
+
+// Déclaration de la fonction execute_command
+void execute_command(char *command);
+
+#endif /* SHELL_H */
+
