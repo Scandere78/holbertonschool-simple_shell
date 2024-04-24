450050e8ee269062e34de292c0a193f4edf337d4
Scandere78 <scanderetejpro@gmail.com>
2024-04-24 13:25:43 +0200

    fix bug

--- main.c
+++ main.c
@@ -10,39 +10,25 @@ int main(int argc, char **argv, char **env)
 	char *line;
 	char **args;
 	int status;
-
 	(void)argc;
 	(void)argv;
-
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
 			printf("$ ");
-
 		fflush(stdout);
-
 		line = get_line();
-
 		if (line == NULL)
 		{
 			break;
 		}
-
 		args = split_line(line);
-
 		if (args[0] != NULL)
 		{
-
 			pid_t pid = fork();
-
 			if (pid == -1)
 			{
 				perror("fork");
-			}
-			if (command_exists(args[0]) == 0)
-			{
-				printf("%s: command not found\n", args[0]);
-				continue;
 			}
 			else if (pid == 0)
 			{
@@ -52,13 +38,9 @@ int main(int argc, char **argv, char **env)
 			{
 				waitpid(pid, &status, WUNTRACED);
 			}
-
-
 			free(line);
 			free(args);
 		}
 	}
-
 	return (0);
 }
-
--- shell.h
+++ shell.h
@@ -7,8 +7,10 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+#define MAX_COMMAND_LENGTH 1024
 char *get_line(void);
 void execute_command(char **args, char **envp);
 char **split_line(char *line);
 char *get_path(char **envp, char *cmd_name);
+int command_exists(char *command);
 #endif
